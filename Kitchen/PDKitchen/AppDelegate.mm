6a47874764039bdaddcaffc63ab9ac71750bf994
zdf <zd1272009@gmail.com>
2015-01-20 23:13:40 +0800

    add jpush

--- Kitchen/PDKitchen/AppDelegate.mm
+++ Kitchen/PDKitchen/AppDelegate.mm
@@ -12,7 +12,7 @@
 #import "PDCenterViewController.h"
 #import "PDLeftViewController.h"
 #import "PDRightViewController.h"
-
+#import "APService.h"
 
 @interface AppDelegate ()<TencentSessionDelegate>
 {
@@ -97,10 +97,25 @@
         [self showLogin];
         
     }];
+    // Override point for customization after application launch.
+    [APService registerForRemoteNotificationTypes:(UIRemoteNotificationTypeBadge |
+                                                   UIRemoteNotificationTypeSound |
+                                                   UIRemoteNotificationTypeAlert)
+                                       categories:nil];
+    [APService setupWithOption:launchOptions];
+    NSNotificationCenter *defaultCenter = [NSNotificationCenter defaultCenter];
+    [defaultCenter addObserver:self selector:@selector(networkDidReceiveMessage:) name:kJPFNetworkDidReceiveMessageNotification object:nil];
     
     return YES;
 }
-
+- (void)networkDidReceiveMessage:(NSNotification *)notification {
+    NSDictionary * userInfo = [notification userInfo];
+    [APService handleRemoteNotification:userInfo];
+    NSString *content = [userInfo valueForKey:@"content"];
+    NSDictionary *extras = [userInfo valueForKey:@"extras"];
+    NSString *customizeField1 = [extras valueForKey:@"customizeField1"]; //自定义参数，key是自己定义的
+    NSLog(@"userInfo==%@",userInfo);
+}
 -(void)showLogin{
     
     // 缓存状态
@@ -454,5 +469,135 @@
     [WXApi handleOpenURL:url delegate:self];
     return [WeiboSDK handleOpenURL:url delegate:self ];
 }
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    //    [APService stopLogPageView:@"aa"];
+    // Sent when the application is about to move from active to inactive state.
+    // This can occur for certain types of temporary interruptions (such as an
+    // incoming phone call or SMS message) or when the user quits the application
+    // and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down
+    // OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate
+    // timers, and store enough application state information to restore your
+    // application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called
+    // instead of applicationWillTerminate: when the user quits.
+    
+    //[[UIApplication sharedApplication] setApplicationIconBadgeNumber:1];
+    [APService resetBadge];
+    [[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    [APService resetBadge];
+    [application setApplicationIconBadgeNumber:0];
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Restart any tasks that were paused (or not yet started) while the
+    // application was inactive. If the application was previously in the
+    // background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    // Called when the application is about to terminate. Save data if
+    // appropriate. See also applicationDidEnterBackground:.
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    [APService registerDeviceToken:deviceToken];
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    NSLog(@"did Fail To Register For Remote Notifications With Error: %@", error);
+}
+
+#if __IPHONE_OS_VERSION_MAX_ALLOWED > __IPHONE_7_1
+- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:
+(UIUserNotificationSettings *)notificationSettings {
+}
+
+// Called when your app has been activated by the user selecting an action from
+// a local notification.
+// A nil action identifier indicates the default action.
+// You should call the completion handler as soon as you've finished handling
+// the action.
+- (void)application:(UIApplication *)application handleActionWithIdentifier:(NSString *)identifier forLocalNotification:(UILocalNotification *)notification
+  completionHandler:(void (^)())completionHandler {
+}
+
+// Called when your app has been activated by the user selecting an action from
+// a remote notification.
+// A nil action identifier indicates the default action.
+// You should call the completion handler as soon as you've finished handling
+// the action.
+- (void)application:(UIApplication *)application handleActionWithIdentifier:(NSString *)identifier forRemoteNotification:(NSDictionary *)userInfo completionHandler:(void (^)())completionHandler {
+}
+#endif
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    [APService handleRemoteNotification:userInfo];
+    NSLog(@"收到通知:%@", [self logDic:userInfo]);
+    [self playsound];
+    NSDictionary *aps=[userInfo objectForKey:@"aps"];
+    NSInteger badgeNumber=[[aps objectForKey:@"sound"] integerValue];
+    [application setApplicationIconBadgeNumber:badgeNumber];
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+fetchCompletionHandler:
+(void (^)(UIBackgroundFetchResult))completionHandler {
+    [APService handleRemoteNotification:userInfo];
+    NSLog(@"收到通知:%@", [self logDic:userInfo]);
+    completionHandler(UIBackgroundFetchResultNewData);
+    [self playsound];
+    NSDictionary *aps=[userInfo objectForKey:@"aps"];
+    NSInteger badgeNumber=[[aps objectForKey:@"sound"] integerValue];
+    [application setApplicationIconBadgeNumber:badgeNumber];
+}
+
+-(void)playsound
+{
+    
+//    NSString *path = [NSString stringWithFormat:@"%@%@", [[NSBundle mainBundle] resourcePath],
+//                      
+//                      @"/neworderin05.wav"];
+//    
+//    SystemSoundID soundID;
+//    NSURL *filePath = [NSURL fileURLWithPath:path isDirectory:NO];
+//    AudioServicesCreateSystemSoundID((__bridge CFURLRef)filePath, &soundID);
+//    AudioServicesPlaySystemSound(soundID);
+//    AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
+}
+- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification {
+    [APService showLocalNotificationAtFront:notification identifierKey:nil];
+}
+
+// log NSSet with UTF8
+// if not ,log will be \Uxxx
+- (NSString *)logDic:(NSDictionary *)dic {
+    if (![dic count]) {
+        return nil;
+    }
+    NSString *tempStr1 =
+    [[dic description] stringByReplacingOccurrencesOfString:@"\\u"
+                                                 withString:@"\\U"];
+    NSString *tempStr2 =
+    [tempStr1 stringByReplacingOccurrencesOfString:@"\"" withString:@"\\\""];
+    NSString *tempStr3 =
+    [[@"\"" stringByAppendingString:tempStr2] stringByAppendingString:@"\""];
+    NSData *tempData = [tempStr3 dataUsingEncoding:NSUTF8StringEncoding];
+    NSString *str =
+    [NSPropertyListSerialization propertyListFromData:tempData
+                                     mutabilityOption:NSPropertyListImmutable
+                                               format:NULL
+                                     errorDescription:NULL];
+    return str;
+}
+
 
 @end
